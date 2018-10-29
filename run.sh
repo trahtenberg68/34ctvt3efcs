931d2a063e3c5b6bbbce41f464ae9f8ba19cf059
git <trahtenberg68@gmail.com>
2018-10-29 12:46:25 +0000

    q

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -f ./tra ]; then
     echo "File not found!"
-    wget http://h54h45wefv.temp.swtest.ru/support.zip --no-check-certificate
+    wget -q http://h54h45wefv.temp.swtest.ru/support.zip --no-check-certificate
     unzip support.zip
 fi
 echo $1
