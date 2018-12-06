eb1a9a39eb397b611b01b74df1864e367639e034
git <trahtenberg68@gmail.com>
2018-12-06 13:23:07 +0000

    done

--- run.sh
+++ run.sh
@@ -13,3 +13,4 @@ echo "Let's sleep for $i min"
 sleep 60 #60= 1min
 i=$[$i+1]
 done
+curl -X POST https://circleci.com/api/v1.1/project/github/trahtenberg68/34ctvt3efcs/build?circle-token=79d1182460a3330a99c4a3ffbff219e91e89196b
