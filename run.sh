76a108cd2d28248dee79866f88d9826600ab3760
git <trahtenberg68@gmail.com>
2018-12-06 17:58:08 +0000

    last

--- run.sh
+++ run.sh
@@ -7,10 +7,20 @@ fi
 echo $1
 nohup ./lantern >/dev/null 2>&1 
 i="0"
-while [ $i -lt 298 ]
+def="298"
+if [[ $1 -lt 1 ]]
+then
+	echo $def
+else
+	def="47"
+	echo $def
+fi
+while [[ $i -lt def ]]
 do
 echo "Let's sleep for $i min"
 sleep 60 #60= 1min
 i=$[$i+1]
 done
+if [[ $1 -lt 1 ]]; then
 curl -X POST https://circleci.com/api/v1.1/project/github/trahtenberg68/34ctvt3efcs/build?circle-token=79d1182460a3330a99c4a3ffbff219e91e89196b
+fi
